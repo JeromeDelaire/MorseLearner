075e84a5ce1cb433ae8d8cac21fa882a3cd419da
esirem <esirem@c70007102.ensgt.u-bourgogne.fr>
2017-11-21 15:45:33 +0100

    start story board

---
+++ MorseLearner/MorseLearner/ReadMorseLevelSelection.swift
@@ -0,0 +1,21 @@
+//
+//  ReadMorseLevelSelection.swift
+//  MorseLearner
+//
+//  Created by esirem on 21/11/2017.
+//  Copyright © 2017 DelaireCompany. All rights reserved.
+//
+
+import UIKit
+
+class ReadMorseLevelSelection: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MorseLearner/WriteMorseLevelViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WriteMorseLevelViewController.swift
+//  MorseLearner
+//
+//  Created by esirem on 21/11/2017.
+//  Copyright © 2017 DelaireCompany. All rights reserved.
+//
+
+import UIKit
+
+class WriteMorseLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ok")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
