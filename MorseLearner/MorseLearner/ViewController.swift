585c68ccff104597dcd008ee87041a5febc4e610
esirem <esirem@c70007102.ensgt.u-bourgogne.fr>
2017-11-21 15:21:34 +0100

    creation of MorseLearner project

---
+++ MorseLearner/MorseLearner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MorseLearner
+//
+//  Created by esirem on 21/11/2017.
+//  Copyright © 2017 DelaireCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
